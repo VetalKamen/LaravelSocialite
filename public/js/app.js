5853481852e760e3ec3c1785cd3f27c60ea74980
Vitalii Kemeniash <Vitalii_Kemeniash@epam.com>
2021-04-26 23:20:54 +0300

    Simple Laravel Library Application with OAuth 1.0

--- public/js/app.js
+++ public/js/app.js
@@ -49878,8 +49878,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\BookShop\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\BookShop\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Mentoring2021_projects\laravelTasks\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Mentoring2021_projects\laravelTasks\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/','BookController@index')->name('books');
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback','Auth\LoginController@handleProviderCallback');
 Route::get('/admin','BookController@admin')->name('admin.books');
 Route::get('/admin/author','AuthorController@admin')->name('admin.authors');
 Route::resource('book', 'BookController');
 Route::resource('author', 'AuthorController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
